e5b74423a6db431dd61ed3f02de863e3fe46fe9b
limcuili <limcuili@hotmail.com>
2018-07-25 16:46:32 +0100

    /name path

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,9 +1,11 @@
 feature 'Player enters name' do
-  scenario 'Can accept player input and redirect path' do
+
+  scenario 'accepts player input and redirect path' do
     visit ('/')
     fill_in :player_1_name, with: 'Player 1'
     fill_in :player_2_name, with: 'Player 2'
     click_button 'Submit'
     expect(page).to have_content 'Player 1 vs. Player 2'
   end
+
 end
